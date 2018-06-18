86334a218547b101643194fe992a37487cf828be
stasbar <stasbar1995@gmail.com>
2018-06-18 15:13:38 +0200

    config frontend <-> server <-> manager

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to RaspberryWallet</h1>
-        </header>
-        <p className="App-intro">
-          RaspberryWallet
-        </p>
-      </div>
-    );
-  }
+
+
+    state = {
+        response : null
+    };
+
+    componentDidMount() {
+        axios.get(`/ping`)
+            .then(res => {
+                const response = res.data;
+                this.setState({ response });
+            })
+    }
+
+    render() {
+        const { response } = this.state;
+
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Welcome to RaspberryWallet</h1>
+                </header>
+
+                <p className="App-intro">
+                    RaspberryWallet
+                </p>
+                {response && <p>
+                    Response from server: {response}
+                </p>
+                }
+
+            </div>
+        );
+    }
 }
 
 export default App;
